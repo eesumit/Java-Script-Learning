865a469816ab2a6c6e325eb17c523769c9226347
SUMIT <112788729+eesumit@users.noreply.github.com>
2023-11-24 05:03:13 +0000

    conversion of datatypes

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,28 @@
-console.log("sumit");
+"use strict" // --> for newer version
+let accountId = 1234
+let accountPassword = "sohna@001"
+const accountName = "sumit"
+// console.table([accountId,accountName,accountPassword,x])
+
+
+let string = "123a"
+let number = Number(string)
+// console.log(typeof number)
+// console.log(typeof string)
+// console.log(number)//---> NaN
+
+let logIn = true;
+let number2 = Number(logIn)
+// console.log(typeof logIn)
+// console.log(typeof number2)
+// console.log(number2)
+
+let x = 2;
+let booleanX = Boolean(x);
+// console.log(typeof x)
+// console.log(typeof booleanX)
+// console.log(booleanX)
+
+console.log(typeof NaN)//--> number
+console.log(typeof undefined)//-->Undefined
+console.log(typeof null)//-->object
