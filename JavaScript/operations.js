11e42fcf47600d5a678c5f17c3cb0f5194c54f74
SUMIT <112788729+eesumit@users.noreply.github.com>
2023-11-24 05:36:30 +0000

    these are typeof

--- JavaScript/operations.js
+++ JavaScript/operations.js
@@ -1,3 +1,24 @@
 let value = 3
 //7 Primitive Data Types:- String Number Boolean null Undefined Symbol BigInt
 //3 Non-Primitive data types:- Array,Function,Object
+/* typeof null --> Object
+   typeof Object --> Object
+   typeof Array --> Object
+   typeof function --> function
+   typeof Undefined --> Undefined
+   typeof Symbol --> Symbol
+  */
+ const number = {
+    X : 3,
+    name : "sumit",
+    city : "jaipur"
+ }
+ console.log(typeof number)
+
+ let array = [1,2,3,4,true,"sumit"]
+ console.log(typeof array)
+
+ function sumit(){
+    console.log(" Hare Ram!")
+ }
+ console.log(typeof sumit)
